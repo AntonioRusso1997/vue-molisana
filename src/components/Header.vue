1a03613e251cac3ffac1b4eebba6f488970a0936
Antonio <antonioru1997@gmail.com>
2021-09-20 11:37:04 +0200

    Scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>sono l'Header</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Sezione prodotti interno del mail</h1>
+</template>
+
+<script>
+export default {
+  name: 'Products',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
